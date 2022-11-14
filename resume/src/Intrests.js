a774b0291816345621505f7d973f2a642be4eb60
1010varun <varunagarwal10.2@gmail.com>
2022-11-14 15:49:04 +0530

    corrected intrest

--- resume/src/Intrests.js
+++ resume/src/Intrests.js
@@ -6,9 +6,8 @@ const Intrests = () => {
             <section class="resume-section" id="interests">
                 <div class="resume-section-content">
                     <h2 class="mb-5">Interests</h2>
-                    <p>Apart from being a web developer, I enjoy most of my time being outdoors. In the winter, I am an avid skier and novice ice climber. During the warmer months here in Rampur, I enjoy free climbing, 
-                        cycling and kayaking.</p>
-                    <p class="mb-0">When forced indoors, I follow a number of sci-fi and fantasy genre movies and television shows, I am an aspiring chef, and I spend a large amount of my free time exploring the latest technology advancements in the front-end web development world.</p>
+                    <p>Apart from being a web developer, I enjoy most of my time being outdoors. I like travelling a lot with friendsand explore many places.</p>
+                    <p class="mb-0">When forced indoors, I follow a number of sci-fi and fantasy genre movies and television shows, I spend a large amount of my free time exploring the latest technology advancements in the front-end and back-end web development world.</p>
                 </div>
             </section>
             <hr class="m-0" />
