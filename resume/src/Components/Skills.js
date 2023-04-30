707d574a1eda081184ff1d820a68cb037eb415bd
1010varun <varunagarwal10.2@gmail.com>
2023-04-30 17:32:05 +0530

    updated skills

--- resume/src/Components/Skills.js
+++ resume/src/Components/Skills.js
@@ -6,12 +6,17 @@ import {
   FaBootstrap,
   FaReact,
   FaPython,
+  FaAws,
+  FaDocker,
+  FaGithub,
 } from "react-icons/fa";
+import { DiDjango } from "react-icons/di";
 import {
   SiTailwindcss,
   SiJavascript,
   SiMongodb,
   SiExpress,
+  SiPostman,
 } from "react-icons/si";
 
 const Skills = () => {
@@ -20,56 +25,92 @@ const Skills = () => {
         <section class="resume-section" id="skills">
           <div class="resume-section-content">
             <h2 class="mb-5">Skills</h2>
-            <div class="subheading mb-3">Programming Languages & Tools</div>
+            <div class="subheading mb-3">Programming Languages</div>
             <ul class="list-inline dev-icons">
               <li class="list-inline-item">
-                <i>
+                <i title="HTML">
                   <FaHtml5 />
                 </i>
               </li>
               <li class="list-inline-item">
-                <i>
+                <i title="CSS">
                   <FaCss3Alt />
                 </i>
               </li>
               <li class="list-inline-item">
-                <i>
-                  <FaBootstrap />
-                </i>
-              </li>
-              <li class="list-inline-item">
-                <i>
-                  <SiTailwindcss />
-                </i>
-              </li>
-              <li class="list-inline-item">
-                <i>
+                <i title="Javascript">
                   <SiJavascript />
                 </i>
               </li>
               <li class="list-inline-item">
-                <i>
+                <i title="Python">
+                  <FaPython />
+                </i>
+              </li>
+              <li class="list-inline-item">
+                <i title="Python">{/* <TbBrandCpp /> */}</i>
+              </li>
+            </ul>
+
+            <div class="subheading mt-5">FrameWorks</div>
+            <ul class="list-inline dev-icons">
+              <li class="list-inline-item">
+                <i title="Bootstrap">
+                  <FaBootstrap />
+                </i>
+              </li>
+              <li class="list-inline-item">
+                <i title="Tailwind CSS">
+                  <SiTailwindcss />
+                </i>
+              </li>
+              <li class="list-inline-item">
+                <i title="React">
                   <FaReact />
                 </i>
               </li>
               <li class="list-inline-item">
-                <i>
+                <i title="NodeJS">
                   <FaNode />
                 </i>
               </li>
               <li class="list-inline-item">
-                <i>
+                <i title="Express">
                   <SiExpress />
                 </i>
               </li>
               <li class="list-inline-item">
-                <i>
+                <i title="Django">
+                  <DiDjango />
+                </i>
+              </li>
+            </ul>
+
+            <div class="subheading mt-5">Tools</div>
+            <ul class="list-inline dev-icons">
+              <li class="list-inline-item">
+                <i title="MongoDB">
                   <SiMongodb />
                 </i>
               </li>
               <li class="list-inline-item">
-                <i>
-                  <FaPython />
+                <i title="AWS">
+                  <FaAws />
+                </i>
+              </li>
+              <li class="list-inline-item">
+                <i title="Docker">
+                  <FaDocker />
+                </i>
+              </li>
+              <li class="list-inline-item">
+                <i title="Github">
+                  <FaGithub />
+                </i>
+              </li>
+              <li class="list-inline-item">
+                <i title="Postman">
+                  <SiPostman />
                 </i>
               </li>
             </ul>
